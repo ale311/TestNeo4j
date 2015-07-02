524cb45a9a1894b98a26595f848a224450832033
ale311 <ale.pulse@gmail.com>
2015-07-02 13:06:44 +0200

    aggiunti nodi Album Problemi con TAG e lastFM

--- src/main/java/prova/JavaAPIExample.java
+++ src/main/java/prova/JavaAPIExample.java
@@ -29,11 +29,11 @@ import de.umass.lastfm.Track;
 import de.umass.lastfm.User;
 
 public class JavaAPIExample{
-	
+
 	private static final String username = "ale_311";
 	private static final String apiKey ="95f57bc8e14bd2eee7f1df8595291493";
 	private static final String DB_PATH = "util/neo4j-community-2.2.3/data/graph.db";
-	
+
 	public enum MieRelazioni implements RelationshipType{
 		ASCOLTA, COMPONE, AMICO, VICINO, TAGGED;
 	}
@@ -41,116 +41,111 @@ public class JavaAPIExample{
 		Date currentDate = new Date();
 		System.out.println( "Starting database ... " + currentDate.getTime() );
 		FileUtils.deleteRecursively( new File( DB_PATH ) );
-		
+
 		HashSet<Track> insiemeTracce = new HashSet<Track>();
- 
+
 		// START SNIPPET: startDb 
 		GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
-		
+
 		ResourceIterator<Node> resultIterator = null;
-		
-		try ( Transaction tx = graphDb.beginTx() ){
-		Label labelUtente = DynamicLabel.label("Utente");
-		Label labelTraccia = DynamicLabel.label("Traccia");
-		Label labelArtista = DynamicLabel.label("Artista");
-		String queryString ="";
-		Map<String,Object> parameters = new HashMap<String, Object>();
-//		
-//		//Accedo a last per estrarre gli amici dell'utente selezionato
-//		PaginatedResult<User> amici = User.getFriends(username, apiKey);
-//		//costruisco relazione tra vicini e utente
-//		for(User u : amici){
-//			String friendName = u.getName();
-//			
-//			queryString = "merge(u1:Utente{Utente:{username}})"+
-//							"merge(u2:Utente{Utente:{vicino}})"+
-//							"merge(u1)-[:AMICO]-(u2)";
-//			parameters.put("username", username);
-//			parameters.put("vicino", friendName);
-//			resultIterator = graphDb.execute(queryString, parameters).columnAs("utente e suoi amici");
-//		
-//		}
-//		parameters.clear();
-//		//scorro la collezione dei vicini e li collego alle tracce ascoltate da loro
-//		for(User u : amici){
-//			String friendName = u.getName();
-//			PaginatedResult<Track> ascoltiDeiVicini = User.getRecentTracks(friendName, 1, 200, apiKey);
-//			for (Track tracciaCorrente : ascoltiDeiVicini){
-//				//inserisco la traccia nel mio SET
-//				insiemeTracce.add(tracciaCorrente);
-//				//collego l'utente alla traccia
-//				String nomeTraccia = tracciaCorrente.getName();
-//				queryString = "merge(u:Utente{Utente:{username}})"+
-//								"merge(t:Traccia{Traccia:{Traccia}})"+	
-//								"merge(u)-[:ASCOLTA]-(t)";
-//				parameters.put("username", friendName);
-//				parameters.put("Traccia", nomeTraccia);
-//				resultIterator = graphDb.execute(queryString, parameters).columnAs("utente ascolta tracce");
-//			}
-//			
-//		}
-		
-		parameters.clear();
-		
-		//Accedo a lastfm per estrarre i max 200 ascolti dell'utente selezionato
-		PaginatedResult<Track> ascoltiDellUtente = User.getRecentTracks(username, 1, 200, apiKey);
-		for (Track tracciaCorrente : ascoltiDellUtente){
-			//inserisco le tracce ascoltate nel mio SET
-			insiemeTracce.add(tracciaCorrente);
-			String nomeTraccia = tracciaCorrente.getName();
-			queryString = "merge(u:Utente{Utente:{username}})"+
-							"merge(t:Traccia{Traccia:{Traccia}})"+	
-							"merge(u)-[:ASCOLTA]-(t)";
-			parameters.put("username", username);
-			parameters.put("Traccia", nomeTraccia);
-			resultIterator = graphDb.execute(queryString, parameters).columnAs("utente ascolta tracce");
-		}
-		parameters.clear();
-		
-		
-		//scansiono la collezione di tracce per associare la relazione COMPONE
-		//con Artista, presa sempre da LAST
-		for (Track tracciaCorrente : insiemeTracce){
-			
-			String nomeTraccia = tracciaCorrente.getName();
-			String artistaTraccia = tracciaCorrente.getArtist();
-			String mbidSTRING = tracciaCorrente.getMbid();
-			
-			queryString = "merge(t:Traccia{Traccia:{Traccia}}) "+ 
-							"merge(a:Artista{Artista:{Artista}})"+
-							"merge(a)-[:COMPONE]-(t)";
-			parameters.put("Traccia", nomeTraccia);
-			parameters.put("Artista", artistaTraccia);
-			resultIterator = graphDb.execute(queryString, parameters).columnAs("artista compone tracce");
-			
-		}
-		parameters.clear();
-		
-		//scansiono la lista di tracce presenti nel mio SET
-		//assegno dei tag per ognuna
-		for(Track tracciaCorrente : insiemeTracce){
-			String nomeTraccia = tracciaCorrente.getName();
-			String artistaTraccia = tracciaCorrente.getArtist();
-			String mbidSTRING = tracciaCorrente.getMbid();
-			
-			Collection<String> tags = Track.getTags(artistaTraccia, nomeTraccia, null);
-			for(String tagCorrente : tags){
+
+		try (Transaction tx = graphDb.beginTx()){
+			Label labelUtente = DynamicLabel.label("Utente");
+			Label labelTraccia = DynamicLabel.label("Traccia");
+			Label labelArtista = DynamicLabel.label("Artista");
+			String queryString ="";
+			Map<String,Object> parameters = new HashMap<String, Object>();
+			//		
+			//		//Accedo a last per estrarre gli amici dell'utente selezionato
+			//		PaginatedResult<User> amici = User.getFriends(username, apiKey);
+			//		//costruisco relazione tra vicini e utente
+			//		for(User u : amici){
+			//			String friendName = u.getName();
+			//			
+			//			queryString = "merge(u1:Utente{Utente:{username}})"+
+			//							"merge(u2:Utente{Utente:{vicino}})"+
+			//							"merge(u1)-[:AMICO]-(u2)";
+			//			parameters.put("username", username);
+			//			parameters.put("vicino", friendName);
+			//			resultIterator = graphDb.execute(queryString, parameters).columnAs("utente e suoi amici");
+			//		
+			//		}
+			//		parameters.clear();
+			//		//scorro la collezione dei vicini e li collego alle tracce ascoltate da loro
+			//		for(User u : amici){
+			//			String friendName = u.getName();
+			//			PaginatedResult<Track> ascoltiDeiVicini = User.getRecentTracks(friendName, 1, 200, apiKey);
+			//			for (Track tracciaCorrente : ascoltiDeiVicini){
+			//				//inserisco la traccia nel mio SET
+			//				insiemeTracce.add(tracciaCorrente);
+			//				//collego l'utente alla traccia
+			//				String nomeTraccia = tracciaCorrente.getName();
+			//				queryString = "merge(u:Utente{Utente:{username}})"+
+			//								"merge(t:Traccia{Traccia:{Traccia}})"+	
+			//								"merge(u)-[:ASCOLTA]-(t)";
+			//				parameters.put("username", friendName);
+			//				parameters.put("Traccia", nomeTraccia);
+			//				resultIterator = graphDb.execute(queryString, parameters).columnAs("utente ascolta tracce");
+			//			}
+			//			
+			//		}
+
+			parameters.clear();
+
+			//Accedo a lastfm per estrarre i max 200 ascolti dell'utente selezionato
+			PaginatedResult<Track> ascoltiDellUtente = User.getRecentTracks(username, 1, 200, apiKey);
+			for (Track tracciaCorrente : ascoltiDellUtente){
+				//inserisco le tracce ascoltate nel mio SET
+				insiemeTracce.add(tracciaCorrente);
+				//grafo: costruisco relazione tra utente e traccia
+				String nomeTraccia = tracciaCorrente.getName();
+				queryString = "merge(u:Utente{Utente:{username}})"+
+						"merge(t:Traccia{Traccia:{Traccia}})"+	
+						"merge(u)-[:ASCOLTA]-(t)";
+				parameters.put("username", username);
+				parameters.put("Traccia", nomeTraccia);
+				resultIterator = graphDb.execute(queryString, parameters).columnAs("utente ascolta tracce");
+			}
+			parameters.clear();
+
+
+			//scansiono la collezione di tracce per associare la relazione COMPONE
+			//con Artista, presa sempre da LAST
+			for (Track tracciaCorrente : insiemeTracce){
 				
-				String tagName = tagCorrente.getName();
-//				if((tagName.equals(""))||(tagName.isEmpty())) tagName="NULL";
-				System.out.println(tagName);
-//				queryString = "merge(t:Traccia{Traccia:{Traccia}}) "+ 
-//						"merge(tag:Tag{Tag:{Tag}})"+
-//						"merge(tag)-[:GENERE]-(t)";
-//				parameters.put("Traccia", nomeTraccia);
-//				parameters.put("Tag", artistaTraccia);
-//				resultIterator = graphDb.execute(queryString, parameters).columnAs("traccia ha genere tag");
-//				
+				String nomeTraccia = tracciaCorrente.getName();
+				String artistaTraccia = tracciaCorrente.getArtist();
+				String mbidSTRING = tracciaCorrente.getMbid();
+				//grafo: costruisco relazione tra artista e traccia che ha composto
+				queryString = "merge(t:Traccia{Traccia:{Traccia}}) "+ 
+						"merge(a:Artista{Artista:{Artista}})"+
+						"merge(a)-[:COMPONE]-(t)";
+				parameters.put("Traccia", nomeTraccia);
+				parameters.put("Artista", artistaTraccia);
+				resultIterator = graphDb.execute(queryString, parameters).columnAs("artista compone tracce");
+
+			}
+			parameters.clear();
+
+			//scansiono la lista di tracce presenti nel mio SET
+			//assegno gli album alle tracce presenti nel SET
+			for(Track tracciaCorrente : insiemeTracce){
+				String nomeTraccia = tracciaCorrente.getName();
+				String artistaTraccia = tracciaCorrente.getArtist();
+				String nomeAlbum = tracciaCorrente.getAlbum();
+				String mbidAlbum = tracciaCorrente.getAlbumMbid();
+				
+				queryString = "merge(t:Traccia{Traccia:{Traccia}})"+
+								"merge(a:Album{Album:{Album}})"+
+								"merge(a)-[:CONTIENE]-(t)";
+				parameters.put("Traccia", nomeTraccia);
+				parameters.put("Album", nomeAlbum);
+				resultIterator = graphDb.execute(queryString, parameters).columnAs("album contiene tracce");
 			}
 			
-		}
-		tx.success();
-		}
 		
+		tx.success();
 	}
+
+}
 }
